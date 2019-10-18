04afb568f697f568e261402f56123bd73e24b195
Bautz, Tom <tom.bautz@outlook.com>
2019-10-18 14:17:51 +0200

    Add test framework

---
+++ src/backend/app.test.js
@@ -0,0 +1,5 @@
+describe('Exmaple Test', () => {
+  test('should be true', () => {
+    expect(true).toBeTruthy()
+  })
+})
---
+++ src/frontend/pages/WelcomePage.test.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils'
+import WelcomePage from './WelcomePage.vue'
+
+describe('WelcomePage', () => {
+  test('should render names', () => {
+    const wrapper = mount(WelcomePage);
+    const h1 = wrapper.find("h1");
+
+    const expectedText = 'Hello Bean, Jonas, Mustafa, Tom!';
+
+    expect(h1.exists()).toBeTruthy();
+    expect(h1.text()).toBe(expectedText);
+  })
+})
