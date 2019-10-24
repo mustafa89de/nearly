cb09c5842ab1a9f054f39f00b0138e591e90ccb5
pagrafy <pagrafy@gmail.com>
2019-10-25 00:04:49 +0200

    formatted RegistrationpPage.text.js

--- src/frontend/pages/RegistrationPage.test.js
+++ src/frontend/pages/RegistrationPage.test.js
@@ -1,14 +1,14 @@
-import { mount } from '@vue/test-utils'
-import WelcomePage from './WelcomePage.vue'
+import { mount } from '@vue/test-utils';
+import RegistrationPage from './RegistrationPage.vue';
 
 describe('RegistrationPage', () => {
   test('should render names', () => {
-    const wrapper = mount(WelcomePage);
+    const wrapper = mount(RegistrationPage);
     const h1 = wrapper.find("h1");
 
     const expectedText = 'Hello Bean, Jonas, Mustafa, Tom!';
 
     expect(h1.exists()).toBeTruthy();
     expect(h1.text()).toBe(expectedText);
-  })
-})
+  });
+});
