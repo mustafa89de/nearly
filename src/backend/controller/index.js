ff40dfb340776abd35eaccbc4a46ff0f38873964
jns-br <jonasdone@gmail.com>
2019-10-30 16:10:46 +0100

    add route to login endpoint

--- src/backend/controller/index.js
+++ src/backend/controller/index.js
@@ -1,9 +1,11 @@
 const UserController = require('./UserController');
+const AuthenticationController = require('./AuthenticationController');
 
 const express = require('express');
 const router = express.Router();
 
 // connect specific routes
 router.use('/user', UserController);  // Path chaining -> /api/user/...
+router.use('/login', AuthenticationController);
 
 module.exports = router;
