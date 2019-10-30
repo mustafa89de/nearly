4ec66d8472cd9c6918c8a7478a39318626b3a35d
jns-br <jonasdone@gmail.com>
2019-10-30 09:27:30 +0100

    add signToken function to generate token based on user

--- src/backend/services/UserService.js
+++ src/backend/services/UserService.js
@@ -1,13 +1,28 @@
 const bcrypt = require('bcrypt');
+const JWT = require('jsonwebtoken');
+const {JWT_SECRET} = require('../configuration/config');
 
 class UserService {
     hashPassword(plainPassword) {
         return bcrypt.hash(plainPassword, 10);
     }
 
-    compareHashed(first, second) {
-        // TODO: implement later
+    compareHashed(inputPassword, dBPassword) {
+        try {
+            return bcrypt.compare(inputPassword, dBPassword);
+
+        } catch (err) {
+            throw new Error(err);
+        }
     }
+
+    signToken = user => {
+        return JWT.sign({
+            iss: 'MeetApp',
+            sub: user.id,
+            exp: new Date().setDate(new Date().getDate() + 1) //current time +1 day
+        }, JWT_SECRET);
+    };
 }
 
 module.exports = new UserService();
