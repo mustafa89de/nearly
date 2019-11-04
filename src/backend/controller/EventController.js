3f3ff0c1eb5f599ff156ea84d2bf7820f8902b8a
jns-br <jonasdone@gmail.com>
2019-11-04 20:15:47 +0100

    add secured endpoint for GET /events

--- src/backend/controller/EventController.js
+++ src/backend/controller/EventController.js
@@ -1,5 +1,6 @@
 const JWTService = require("../services/JWTService");
 const EventRepository = require("../repositories/EventRepository");
+const MapService = require("../services/MapService");
 
 const express = require('express');
 const router = express.Router();
@@ -11,6 +12,16 @@ router.post('/', JWTService.requireJWT(), async (req, res) => {
     const createdEvent = await EventRepository.createEvent(name, latitude, longitude, time, hostId, description);
 
     res.status(201).json(createdEvent);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+});
+
+router.get('/', JWTService.requireJWT(), async (req, res) => {
+  try {
+    const {north, east, west, south} = req.body;
+    const events = MapService.getEventsInArea(north, east, south, west);
+    res.status(201).json(events);
   } catch (err) {
     res.status(500).json({message: err.message});
   }
