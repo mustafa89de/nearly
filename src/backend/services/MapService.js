7e96cbd327e4fe751b24e32d30e16bef6c581d50
jns-br <jonasdone@gmail.com>
2019-11-05 17:43:32 +0100

    add createMissingBounds function

--- src/backend/services/MapService.js
+++ src/backend/services/MapService.js
@@ -1,46 +1,20 @@
-const geolib = require('geolib');
-const EventRepository = require('../repositories/EventRepository');
-
 class MapService{
-  async getEventsInArea(north, east, south, west){
-    try {
-      let area = [
-        {latitude: north.lat, longitude: north.lng},
-        {latitude: east.lat, longitude: east.lng},
-        {latitude: west.lat, longitude: west.lng},
-        {latitude: south.lat, longitude: south.lng},
-      ];
+  createMissingBounds(ne, sw){
+    let se = {
+      lat: sw.lat,
+      lng: ne.lng
+    };
 
-      let events = await EventRepository.getAllEvents();
-      /*
-      events.filter(event => {
-        const eventCoordinates = {
-          latitude: event.latitude,
-          longitude: event.longitude
-        };
-        return geolib.isPointInPolygon(eventCoordinates, area);
-      });
-*/
-      let eventObjects = [];
-      events.forEach(event => {
-        const eventCoordinates = {
-          latitude: event.latitude,
-          longitude: event.longitude
-        };
-        const inArea = geolib.isPointInPolygon(eventCoordinates, area);
-        if(inArea) {
-          eventObjects.push({
-            lon: event.longitude,
-            lat: event.latitude
-          })
-        }
-      });
-      return eventObjects;
+    let nw = {
+      lat: ne.lat,
+      lng: sw.lng
+    };
 
-    } catch (err) {
-      throw err;
-    }
+    return [se, nw];
   }
+
+
+
 }
 
 module.exports = new MapService();
