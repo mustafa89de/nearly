366e5e6e32f1098e33159b3210755c8d25f7b9c6
mustafasafi <mustafa_zein@yahoo.de>
2019-11-05 01:13:07 +0100

    Update: apply design draft on eventdetailpage add: new component "textfield" for text button combination

---
+++ src/frontend/components/TextField.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="content">
+    <icon v-if="iconType" :iconType="iconType" :iconColor="iconColor"/>
+    <span>{{ value }}</span>
+  </div>
+</template>
+
+<script>
+  import Icon from "../components/Icon.vue";
+
+  export default {
+    props: {
+      iconType: String,
+      iconColor: String,
+      value: [String, Number, Date]
+    },
+
+    data: function () {
+      return {
+      };
+    },
+
+    components: {
+      icon: Icon
+    },
+
+    methods: {
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+  @import "../assets/variables";
+  @import "../assets/mixins.scss";
+
+  .content {
+    display: flex;
+    align-items: center;
+
+    svg {
+      flex: none
+    }
+
+    span {
+      @include textBody;
+      flex: 1;
+      padding: 10px;
+      padding-left: 20px;
+      border: 0;
+      font-size: 18px;
+      color: $font-col-light;
+    }
+  }
+
+
+</style>
