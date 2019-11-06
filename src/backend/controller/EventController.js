116e9061da107d37d34e5fc2ecd21bb3ecc5e625
jns-br <jonasdone@gmail.com>
2019-11-06 18:33:02 +0100

    change status to 200

--- src/backend/controller/EventController.js
+++ src/backend/controller/EventController.js
@@ -27,7 +27,7 @@ router.get('/', JWTService.requireJWT(), async (req, res) => {
     const missingBounds = MapService.createMissingBounds(ne, sw);
     const [se, nw] = missingBounds;
     const events = await EventRepository.getEventsInArea(ne, se, sw, nw);
-    res.status(201).json(events);
+    res.json(events);
   } catch (err) {
     res.status(500).json({message: err.message});
   }
