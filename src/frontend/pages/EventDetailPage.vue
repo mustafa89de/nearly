0cb78a09829305e40c1c725dd36b8c4d60f42159
mustafasafi <mustafa_zein@yahoo.de>
2019-11-06 12:56:37 +0100

    added map component to event detail view

--- src/frontend/pages/EventDetailPage.vue
+++ src/frontend/pages/EventDetailPage.vue
@@ -1,19 +1,11 @@
 <template>
-  <div class="event">
-    <div v-if="isLoading" class="eventLoading">
-      Loading...
-    </div>
-    <div v-if="error" class="error">
-      {{error}}
-    </div>
+  <div>
+    <div v-if="isLoading" class="eventLoading"> Loading...</div>
+    <div v-if="error" class="error"> {{error}}</div>
     <div v-if="event" class="eventContent">
       <article>
-        <header>
-          <h1>
-            {{name}}
-          </h1>
-          <!-- Share Button to be implemented here -->
-        </header>
+        <h1> {{name}} </h1>
+        <!-- Share Button to be implemented here -->
         <p class="description">{{ description }}</p>
         <div class="fieldContainer">
           <div class="leftSide">
@@ -29,9 +21,9 @@
         <button-submit v-if="isCreator" @click="editEvent" class="joinButton" type="submit" text="bearbeiten"/>
         <button-submit v-else @click="signInForEvent" class="joinButton" type="submit" text="mitmachen"/>
       </article>
-      <!--
-      <map-comp />
-      -->
+    </div>
+    <div class="mapContainer">
+      <map-comp v-if="event" :initialCenter="mapcenter" :initialZoom="12" :markers="markers"/>
     </div>
   </div>
 </template>
@@ -40,18 +32,18 @@
   import EventService from "../services/EventService";
   import TextField from "../components/TextField";
   import Button from "../components/Button.vue";
-  // import Map from "../components/Map.vue";
-  
+  import Map from "../components/Map.vue";
+
   import UserService from "../services/UserService";
   import AuthService from "../services/AuthService";
 
   export default {
     components: {
       'text-field': TextField,
-      'button-submit': Button
-      // 'map-comp': Map,
+      'button-submit': Button,
+      'map-comp': Map
     },
-    
+
     data() {
       return {
         isLoading: false,
@@ -61,6 +53,8 @@
         description: null,
         latitude: null,
         longitude: null,
+        mapcenter: null,
+        markers: null,
         time: null,
         eventDate: null,
         hostID: null,
@@ -102,6 +96,9 @@
         this.description = event.description;
         this.latitude = event.latitude.$numberDecimal;
         this.longitude = event.longitude.$numberDecimal;
+        this.mapcenter = {longitude: this.longitude, latitude: this.latitude};
+        this.markers = [this.longitude, this.latitude];
+        
         const eventTime = new Date(event.time);
         this.time = eventTime.getHours() + ':' + eventTime.getMinutes();  // to be replaced after aligning with be
         this.eventDate = event.date;
@@ -112,23 +109,23 @@
         if (AuthService.getUser().userId === this.hostID) this.isCreator = true;
         else this.isCreator = false;
       },
-      
-      async editEvent(e){
+
+      async editEvent(e) {
         e.preventDefault();
-        try{
+        try {
           // Routing to Event Editing Page to be implemented here
           await this.$router.push('/');
-        }catch(err){
+        } catch (err) {
           console.error(err);
         }
       },
-      
-      async signInForEvent(e){
+
+      async signInForEvent(e) {
         e.preventDefault();
-        try{
+        try {
           // Routing to Event Signing to be implemented here
           await this.$router.push('/');
-        }catch(err){
+        } catch (err) {
           console.error(err);
         }
       }
@@ -139,20 +136,22 @@
 <style scoped lang="scss">
   @import "../assets/variables";
   @import "../assets/mixins";
-
-  .event {
-    padding-bottom: 20px;
+  
+  .eventContent {
+    position: relative;
+    padding: 25px;
+    background: $bg-col-primary;
     border-bottom-right-radius: 5em;
     border-bottom-left-radius: 5em;
+    z-index: 2;
   }
-
+  
   article {
-    padding: 25px;
     flex: 1;
     display: flex;
     flex-flow: column;
   }
-
+  
   h1 {
     font-family: Poppins, sans-serif;
     font-style: normal;
@@ -162,29 +161,42 @@
     text-transform: uppercase;
     margin: 0;
   }
-
+  
   p.description {
     color: $font-col-primary;
-
+    
     font-size: 18px;
     font-family: Arimo, sans-serif;
     line-height: 21px;
     align-items: flex-end;
     letter-spacing: 0.02em;
-
+    
     transition: 500ms ease;
   }
-
+  
   .fieldContainer {
     display: flex;
     flex: 1;
     justify-content: space-between;
     align-items: center;
   }
-
+  
   .joinButton {
     margin-top: 40px;
     align-self: center;
   }
 
+  .mapContainer{
+    position:absolute;
+    background: #E44021;
+    top: 250px;
+    margin-left: 25px;
+    width: 450px;
+    height: 100vh;
+    z-index: 1;
+  }
+  
+  #map{
+    position:absolute;
+  }
 </style>
