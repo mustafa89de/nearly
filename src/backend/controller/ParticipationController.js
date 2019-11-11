716d7cb82c3891f481ee559eed72f81490c5f0bb
jns-br <jonasdone@gmail.com>
2019-11-11 20:10:04 +0100

    add user check to event participation

--- src/backend/controller/ParticipationController.js
+++ src/backend/controller/ParticipationController.js
@@ -9,11 +9,23 @@ router.post('/', JWTService.requireJWT(), async (req, res) => {
     const {userId, eventId} = req.body;
     const participation = await ParticipationRepository.attendEvent(userId, eventId);
 
-    if(!participation){
-      res.status(404).json({message: `Event with id "${eventId}" does not exist.`});
+    if(participation === null){
+      res.status(404).json({message: `Event with id "${eventId}" 
+      or User with with id "${userId}" does not exist.`});
     }
 
     res.status(201).json();
+  } catch (err) {
+    console.log(err.status);
+    res.status(500).json({message: err.message})
+  }
+});
+
+router.delete('/', JWTService.requireJWT(),async (req, res) => {
+  try {
+    const {userId, eventId} = req.body;
+    await ParticipationRepository.cancelParticipation(userId, eventId);
+    res.json();
   } catch (err) {
     console.log(err.status);
     res.status(500).json({message: err.message})
--- src/backend/repositories/ParticipationRepository.js
+++ src/backend/repositories/ParticipationRepository.js
@@ -1,12 +1,15 @@
 const Participation = require('../models/Participation');
 const Event = require('../models/Event');
+const User = require('../models/User');
 
 class ParticipationRepository {
   async attendEvent(userId, eventId){
     try {
       let event = await Event.findById(eventId);
-      if(!event){
-        return undefined;
+      let user = await User.findById(userId);
+
+      if(!event || !user){
+        return null;
       }
       let participation = Participation({userId, eventId});
       await participation.save();
