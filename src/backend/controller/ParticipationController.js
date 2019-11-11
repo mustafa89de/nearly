be6e6c3849ea4826e1d9d881d123abdf72df6107
jns-br <jonasdone@gmail.com>
2019-11-11 10:28:54 +0100

    add participation controller and post route

---
+++ src/backend/controller/ParticipationController.js
@@ -0,0 +1,23 @@
+const JWTService = require("../services/JWTService");
+const ParticipationRepository = require('../repositories/ParticipationRepository');
+
+const express = require('express');
+const router = express.Router();
+
+router.post('/', JWTService.requireJWT(), async (req, res) => {
+  try {
+    const {userId, eventId} = req.body;
+    const participation = await ParticipationRepository.attendEvent(userId, eventId);
+
+    if(!participation){
+      res.status(404).json({message: `Event with id "${eventId}" does not exist.`});
+    }
+
+    res.status(201).json();
+  } catch (err) {
+    console.log(err.status);
+    res.status(500).json({message: err.message})
+  }
+});
+
+module.exports = router;
