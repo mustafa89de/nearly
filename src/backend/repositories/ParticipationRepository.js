f0752ac03c40c56f3056f08978f3bd5afb72c25d
jns-br <jonasdone@gmail.com>
2019-11-17 15:36:56 +0100

    add getUserParticipations function

--- src/backend/repositories/ParticipationRepository.js
+++ src/backend/repositories/ParticipationRepository.js
@@ -55,6 +55,37 @@ class ParticipationRepository {
       throw err;
     }
   }
+
+  async getUserParticipations(userId){
+    try {
+      const participations = await Participation.find({
+        userId: userId
+      });
+
+      const events = [];
+      if (participations) {
+        for (const participation of participations) {
+          const eventId = participation.eventId;
+          const event = await Event.find({
+            _id: eventId,
+            time: {
+              $gte: new Date()
+            }
+          });
+          events.push(event);
+        }
+      }
+      events.sort((a, b) => {
+        const aTime = a[0]._doc.time;
+        const bTime = b[0]._doc.time;
+        return new Date(aTime) - new Date(bTime);
+      });
+      return events;
+    } catch (err) {
+      console.error('DB Error:', err.message);
+      throw err;
+    }
+  }
 }
 
 module.exports = new ParticipationRepository();
