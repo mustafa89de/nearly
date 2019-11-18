5c85974e89ea95c57e4c07db3b8736c6853bd881
jns-br <jonasdone@gmail.com>
2019-11-18 16:06:33 +0100

    add https server implementation

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+};
 
 const controller = require('./controller');  // imports /controller/index.js with all registered routes
 const DBService = require("./services/DBService");
@@ -12,18 +18,18 @@ app.use(express.json());
 
 app.use('/api', controller); // Path chaining -> /api/...
 
-app.use('*', (req, res) => {
-    res.redirect("https://" + req.headers.host + req.url);
-});
-
 app.use(express.static('dist'));
 
 app.use('*', express.static(path.join(__dirname, '/../../dist/index.html')));
 
+app.use('*', (req, res) => {
+  res.redirect("https://" + req.headers.host + req.url);
+});
+
 Passport.init();
 if (process.env.MODE !== 'TEST') {
   DBService.init();
-  app.listen(PORT, () => {
+  https.createServer(options, app).listen(PORT, () => {
     console.log(`Node app listening on port ${PORT}!`);
   });
 
