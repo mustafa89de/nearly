c4dc38fb417af40f82acb1753f39a33751e9d321
jns-br <jonasdone@gmail.com>
2019-12-04 14:37:23 +0100

    add PushService with subscribeToPush function

---
+++ src/frontend/services/PushService.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import {ENDPOINTS} from "../constants";
+
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+
+class PushService {
+  async subscribeToPush(){
+    try {
+      const registration = await navigator.serviceWorker.register('../worker.js', {
+        scope: '/'
+      });
+      console.log('SW registered');
+
+      const permission = await Notification.requestPermission();
+      if (permission && permission !== 'granted'){
+        console.log('Notification Permission not granted');
+        //TODO: handle permission not granted
+      }
+
+      const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+      });
+      console.log('Subscribed to push');
+
+      const stringifiedSubscription = JSON.stringify(subscription);
+
+      await axios.post(ENDPOINTS.PUSH, {
+        subscription: stringifiedSubscription
+      });
+      console.log('subscription sent to BE');
+
+    }catch (err) {
+      console.error(err.message);
+      throw err;
+    }
+
+  }
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
