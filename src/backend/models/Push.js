fc3a3da2025b491a54715bd48d4e6e92b924dac8
jns-br <jonasdone@gmail.com>
2019-12-04 10:04:58 +0100

    add Push DB-Model

---
+++ src/backend/models/Push.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const pushSchema = mongoose.Schema({
+  userId: {type: String, required: true},
+  subscription: {
+    endpoint: {
+      type: String,
+      required: true
+    },
+    keys: {
+      p256dh: {
+        type: String,
+        required: true
+      },
+      auth: {
+        type: String,
+        required: true
+      }
+    }
+  }
+});
+
+module.exports = mongoose.model('Push', pushSchema);
