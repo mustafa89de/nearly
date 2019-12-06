a7404dc0da0eb4705af1b7afe7a1baa1dcaa7ec5
jns-br <jonasdone@gmail.com>
2019-12-06 10:24:13 +0100

    remove unnecessary urlBase64ToUint8Array function

--- src/frontend/services/PushService.js
+++ src/frontend/services/PushService.js
@@ -21,7 +21,7 @@ class PushService {
 
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+        applicationServerKey: publicVapidKey
       });
       console.log('Subscribed to push');
 
@@ -56,21 +56,6 @@ class PushService {
       throw err;
     }
   }
-}
-
-function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
-
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
 }
 
 export default new PushService();
