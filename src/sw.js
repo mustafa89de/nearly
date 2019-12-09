81d8c02f9d7356ee2225b50a6119158b88dce3d9
mustafasafi <mustafa_zein@yahoo.de>
2019-12-09 21:55:56 +0100

    Add navigation routes to provide basic offline experience

--- src/sw.js
+++ src/sw.js
@@ -3,3 +3,7 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 workbox.precaching.cleanupOutdatedCaches();
 
+// adds navigation routs for SPA
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL('/index.html')
+);
