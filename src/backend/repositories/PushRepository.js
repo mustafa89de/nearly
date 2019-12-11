84c746be670448f3a7283125d2c31fc7de5879e8
jns-br <jonasdone@gmail.com>
2019-12-11 19:48:51 +0100

    add getEventSubscriptions function, returns subscriptions with event within user Radius

--- src/backend/repositories/PushRepository.js
+++ src/backend/repositories/PushRepository.js
@@ -1,4 +1,5 @@
 const PushSubscriptions = require('../models/Push');
+const Event = require('../models/Event');
 
 class PushRepository {
   async saveSubscription(userId, subscription) {
@@ -15,7 +16,7 @@ class PushRepository {
     }
   }
 
-  async deleteSubscription(userId){
+  async deleteSubscription(userId) {
     try {
       const removal = await PushSubscriptions.deleteOne({
         userId
@@ -25,6 +26,37 @@ class PushRepository {
 
     } catch (err) {
       console.error('DB Error:', err.message);
+      throw err;
+    }
+  }
+
+  async getEventSubscriptions(eventId) {
+    try {
+      const allSubscriptions = await PushSubscriptions.find().populate('userId');
+
+      let relevantSubscriptions = (await Promise.all(
+        allSubscriptions.map(async (subscription) => {
+          const userLocation = subscription._doc.userId._doc.homePosition.coordinates;
+          const lng = userLocation[0];
+          const lat = userLocation[1];
+          const radius = 1;
+          const pushEvent = await Event.find({
+            _id: eventId,
+            loc: {
+              $geoWithin: {
+                $centerSphere: [[lng, lat], radius / 6378.1]
+              }
+            }
+          });
+          if (pushEvent.length > 0) {
+            return subscription._doc.subscription;
+          }
+        })
+      )).filter(item => item !== undefined);
+
+      return relevantSubscriptions;
+    } catch (err) {
+      console.error(err.message);
       throw err;
     }
   }
