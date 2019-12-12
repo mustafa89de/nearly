5c73a4a2b6b2c172bab25ad773f3cc4983a5b1ed
jns-br <jonasdone@gmail.com>
2019-12-12 15:10:12 +0100

    update worker to open event page on click

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,14 +1,19 @@
 self.addEventListener('push', e => {
   const data = e.data.json();
+  console.log(data);
   self.registration.showNotification(data.title, {
     body: data.body,
-    data: data.eventId,
+    data: data.data,
     requireInteraction: true,
     vibrate: [300, 100, 300]
   })
+
 });
 
 self.addEventListener('notificationclick', e => {
   //TODO: redirect to event
-  console.log('notification clicked')
+  console.log('notification clicked');
+  clients.openWindow('localhost:3000/event/' + e.notification.data);
+  console.log('opened window')
 });
+
