d793620dc4a8fd8647ca5d360a8b2b331ef636d3
jns-br <jonasdone@gmail.com>
2019-12-12 21:52:44 +0100

    add promise handling to push listener

--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,19 +1,21 @@
-self.addEventListener('push', e => {
-  const data = e.data.json();
-  console.log(data);
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    data: data.data,
-    requireInteraction: true,
-    vibrate: [300, 100, 300]
-  })
-
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      data: data.data,
+      requireInteraction: true,
+      vibrate: [300, 100, 300]
+    })
+  );
 });
 
-self.addEventListener('notificationclick', e => {
-  //TODO: redirect to event
+self.addEventListener('notificationclick', event => {
   console.log('notification clicked');
-  clients.openWindow('localhost:3000/event/' + e.notification.data);
+
+  event.notification.close();
+  clients.openWindow('localhost:3000/event/' + event.notification.data);
+
   console.log('opened window')
 });
 
