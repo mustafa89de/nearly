5c32d21611531edf2e1390119555696ad93d5cae
jns-br <jonasdone@gmail.com>
2019-12-12 12:24:25 +0100

    add notfiyUsers function

--- src/backend/services/PushService.js
+++ src/backend/services/PushService.js
@@ -1,4 +1,5 @@
 const webPush = require('web-push');
+const PushRepository = require('../repositories/PushRepository');
 
 //// generate in terminal with ./node_modules/.bin/web-push generate-vapid-keys
 const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
@@ -16,6 +17,19 @@ class PushService {
   async sendPush(subscription, payload){
     try {
       await webPush.sendNotification(subscription, payload)
+    }catch (err) {
+      console.error('Failed to send push notificaiton: ' + err.message)
+    }
+  }
+
+  async notifyUsers(name, eventId){
+    try {
+      const payload = JSON.stringify({title: 'New Nearly Event', body: name, data: eventId});
+      const subscriptions = await PushRepository.getEventSubscriptions(eventId);
+
+      subscriptions.forEach(async subscription => {
+        await webPush.sendNotification(subscription, payload);
+      });
     }catch (err) {
       console.error('Failed to send push notificaiton: ' + err.message)
     }
