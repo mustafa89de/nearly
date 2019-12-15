8d2b0fa6f85c84be0b2959c9affc004aa27dae4b
Tom Bautz <tom.bautz@axelspringer.de>
2019-12-15 13:57:56 +0100

    Small cleanup and fixes (#105)
    * move public assets to frontend dir, add favicon, fix deprecation warnings, fix wrong icon paths, improve html head
    
    * fix indention
    
    mi-248
    
    * Update src/frontend/index.html

--- src/backend/app.js
+++ src/backend/app.js
@@ -11,8 +11,8 @@ const Passport = require('./passport');
 app.use(express.json());
 
 app.use('*', (req, res, next) => {
-  if (req.host !== 'localhost' && !req.secure && req.get('x-forwarded-proto') !== 'https') {
-    res.redirect("https://" + req.headers.host + req.url);
+  if (req.hostname !== 'localhost' && !req.secure && req.get('x-forwarded-proto') !== 'https') {
+    res.redirect("https://" + req.get('host') + req.url);
   } else {
     next();
   }
