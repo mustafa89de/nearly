7dc981aa950d6df3f755c5dd97c7194e448cc900
Bautz, Tom <tom.bautz@outlook.com>
2019-12-17 17:45:11 +0100

    replace apple symbol font with svg
    no-ticket

--- src/frontend/components/PWANotification.vue
+++ src/frontend/components/PWANotification.vue
@@ -1,14 +1,23 @@
 <template>
   <section v-if="showNotification">
-    Um diese Web App zu installieren, tippe <span>􀈂</span> und dann auf "Zum Home-Bildschirm <span>􀃜</span>"
-    <span @click="closeNotification" id="close">􀆄</span>
+    Um diese Web App zu installieren, tippe
+    <icon iconType="ios-share"/>
+    und dann auf "Zum Home-Bildschirm
+    <icon iconType="ios-add"/>
+    "
+    <icon @click="closeNotification" id="close" iconType="ios-close"/>
   </section>
 </template>
 
 <script>
   import PWAService from "../services/PWAService";
+  import Icon from "../components/Icon.vue";
 
   export default {
+
+    components: {
+      icon: Icon
+    },
     data: function () {
       return {
         showNotification: PWAService.shouldShowNotification()
@@ -43,18 +52,29 @@
     font-family: "Arimo", Arial, Helvetica, sans-serif;
     font-size: 18px;
 
-    > span {
-      font-family: "SF Pro Text";
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: baseline;
+
+    > svg {
+      width: 16px;
 
       &:first-of-type {
-        color: #007aff;
+        fill: #007aff;
+        align-self: end;
+      }
+
+      &:not(#close) {
+        margin: 0 2px 0 5px;
       }
 
       &#close {
         position: absolute;
         top: 10px;
         right: 10px;
-        color: #777;
+        fill: #777;
+        width: 14px;
       }
     }
 
