88193056541b632c7d97339de99892a79cc7f224
mustafasafi <mustafa_zein@yahoo.de>
2019-12-18 11:57:51 +0100

    Create initial offline Page

---
+++ src/frontend/pages/OfflinePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <article>
+    <h1>
+      No Network Connection please try again later!
+    </h1>
+    <div></div>
+  </article>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+  div{
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    background-color: white;
+  }
+</style>
