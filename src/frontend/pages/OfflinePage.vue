c8b5961f2671c2be52d55f49be5d7e45008812c5
mustafasafi <mustafa_zein@yahoo.de>
2019-12-18 20:48:47 +0100

    Finish offline page

--- src/frontend/pages/OfflinePage.vue
+++ src/frontend/pages/OfflinePage.vue
@@ -3,16 +3,37 @@
     <h1>No Internet!</h1>
     <h2>Hoppla!</h2>
     <p>Leider scheinst du kein Internet zu haben! </p>
+    <reload-button
+        type= 'button'
+        text = "Let me back!"
+        @click= handleReloadButton
+        :bordered= "false"
+    />
+    <img src="static/img/offline.gif" />
     <div></div>
   </article>
 </template>
 
 <script>
   export default {
+    components:{
+      'reload-button': Button
+    },
+    
+    methods:{
+      handleReloadButton(){
+        this.$router.push('/');
+      }
+    },
+
     created(){
-      window.addEventListener('online', (e) => this.$router.push('/'))
+      window.addEventListener('online', (e) => {
+        this.$router.push('/')
+      })
     }
   }
+  
+  import Button from "../components/Button"
 </script>
 
 <style scoped lang="scss">
@@ -48,7 +69,7 @@
     p {
       @include textBody;
       color: $font-col-primary;
-      margin: 25px 0 auto 0;
+      margin: 25px 0 25px 0;
     }
   }
   
