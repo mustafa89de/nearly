acdc0e4f8549276e9fa131787d2eca811541ba41
jns-br <jonasdone@gmail.com>
2020-01-11 13:51:24 +0100

    update delete push route to include fingerprint

--- src/backend/controller/PushController.js
+++ src/backend/controller/PushController.js
@@ -19,7 +19,9 @@ router.post('/', JWTService.requireJWT(), async (req, res) => {
 router.delete('/', JWTService.requireJWT(), async (req, res) => {
   try {
     const userId = req.query.id;
-    const removal = await PushRepository.deleteSubscription(userId);
+    const fingerprint = req.query.fingerprint;
+
+    const removal = await PushRepository.deleteSubscription(userId, fingerprint);
     if (!removal) {
       res.status(404).json({message: `Deletion of subscription of user ${userId} unsuccessful`});
     }
